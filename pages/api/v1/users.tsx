34b8e5c236f7b274d423a704dbf040747597e711
Burt <2902978956@qq.com>
2020-09-21 19:54:26 +0800

    加入后端校验 username 唯一性逻辑，并把校验逻辑放到 User.ts

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -3,53 +3,24 @@ import {getDatabaseConnection} from '../../../lib/getDatebaseConnection';
 import {User} from '../../../src/entity/User';
 import md5 from 'md5';
 
-const Posts:NextApiHandler = async (req, res)=>{
-  const {username,password,passwordConfirmation} = req.body
+const Posts: NextApiHandler = async (req, res) => {
+  const {username, password, passwordConfirmation} = req.body;
+  const connection = await getDatabaseConnection();
+  res.setHeader('Content-Type', 'application/json;charset=utf-8');
 
-  const errors = {
-    username:[] as string[],password:[] as string[],passwordConfirmation:[] as string[]
+  const user = new User();
+  user.username = username.trim();
+  user.password = password;
+  user.passwordDigest = passwordConfirmation;
+  await user.validate();
+  if (user.hasErrors()) {
+    res.statusCode = 422;
+    res.write(JSON.stringify(user.errors));
+  } else {
+    await connection.manager.save(user);
+    res.statusCode = 200;
+    res.write(JSON.stringify(user));
   }
-  if(username.trim()===''){
-    errors.username.push('用户名不能为空')
-  }
-  if(!/[a-zA-Z0-9]/.test(username.trim())){
-    errors.username.push('用户名格式不合法')
-  }
-  if(username.trim().length > 16){
-    errors.username.push('用户名太长')
-  }
-  if(username.trim().length < 4){
-    errors.username.push('用户名太短')
-  }
-
-  if(password === ''){
-    errors.password.push('密码不能为空')
-  }
-  if(password.length < 6){
-    errors.password.push('密码太短')
-  }
-  if(password.length > 16){
-    errors.password.push('密码太长')
-  }
-  if(password !== passwordConfirmation){
-    errors.passwordConfirmation.push('密码不匹配')
-  }
-
-  const hasErrors = Object.values(errors).find(v=>v.length>0)
-  res.setHeader('Content-Type','application/json;charset=utf-8')
-  if(hasErrors){
-    res.statusCode = 422
-    res.write(JSON.stringify(errors))
-  }else{
-    const connection = await getDatabaseConnection()
-    const user = new User()
-    user.username = username.trim()
-    user.passwordDigest = md5(password)
-    await connection.manager.save(user)
-    res.statusCode = 200
-    res.write(JSON.stringify(user))
-  }
-  res.end()
-
-}
-export default Posts
+  res.end();
+};
+export default Posts;
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,6 +2,7 @@ import {Entity} from 'typeorm';
 import {Column, CreateDateColumn, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm/index';
 import {Comment} from './Comment';
 import {Post} from './Post';
+import {getDatabaseConnection} from '../../lib/getDatebaseConnection';
 
 @Entity('users')
 export class User {
@@ -21,4 +22,50 @@ export class User {
   comments: Comment[];
   @OneToMany(type => Post, post => post.author)
   posts: Post[];
+
+  errors = {
+    username: [] as string[],
+    password: [] as string[],
+    passwordConfirmation: [] as string[]
+  };
+  password: string;
+  passwordConfirmation: string;
+
+  async validate() {
+    const found = await (await getDatabaseConnection()).manager.find(
+      User, {username: this.username}
+    );
+    if (found) {
+      this.errors.username.push('用户名已存在，不能重复注册');
+    }
+    if (this.username.trim() === '') {
+      this.errors.username.push('用户名不能为空');
+    }
+    if (!/[a-zA-Z0-9]/.test(this.username.trim())) {
+      this.errors.username.push('用户名格式不合法');
+    }
+    if (this.username.trim().length > 16) {
+      this.errors.username.push('用户名太长');
+    }
+    if (this.username.trim().length < 4) {
+      this.errors.username.push('用户名太短');
+    }
+
+    if (this.password === '') {
+      this.errors.password.push('密码不能为空');
+    }
+    if (this.password.length < 6) {
+      this.errors.password.push('密码太短');
+    }
+    if (this.password.length > 16) {
+      this.errors.password.push('密码太长');
+    }
+    if (this.password !== this.passwordConfirmation) {
+      this.errors.passwordConfirmation.push('密码不匹配');
+    }
+  }
+
+  hasErrors() {
+    return !!Object.values(this.errors).find(v => v.length > 0);
+  }
 }
