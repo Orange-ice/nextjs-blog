af99c0f686f6b44e0ef08cf42749acb81007bb6b
Burt <2902978956@qq.com>
2020-09-24 21:58:07 +0800

    修复一些小问题

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,18 +1,13 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 import * as next from 'next'
+// d.ts 文件如果有 import 就不是全局类型声明了
 declare module "*.png" {
   const value: string;
   export default value;
 }
 
-type Post = {
-  id: string;
-  date: string;
-  title: string;
-  content: string;
-  htmlContent: string;
-}
+
 
 declare module 'next' {
   import {Session} from 'next-iron-session';
