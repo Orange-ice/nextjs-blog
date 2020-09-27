34ade49e3a9073541e88026834b71a2e17cc1eb0
Burt <2902978956@qq.com>
2020-09-27 21:10:13 +0800

    完成文章详情页CSS

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {getPost, getPostIds} from '../../lib/posts';
 import {GetServerSideProps, NextPage} from 'next';
 import {getDatabaseConnection} from '../../lib/getDatebaseConnection';
 import {Post} from '../../src/entity/Post';
+import marked from 'marked';
 
 type Props = {
   post: Post
@@ -10,22 +10,39 @@ type Props = {
 const postsShow: NextPage<Props> = (props) => {
   const {post} = props;
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <article dangerouslySetInnerHTML={   {__html: post.content}  }>
-      </article>
-    </div>
+    <>
+      <div className="wrapper">
+        <h1>{post.title}</h1>
+        <article className="markdown-body" dangerouslySetInnerHTML={ {__html: marked(post.content)} }>
+        </article>
+      </div>
+
+      <style jsx>{`
+        .wrapper{
+          max-width: 800px;
+          margin: 16px auto;
+          padding: 0 16px;
+        }
+        h1{
+          padding-bottom: 16px;
+          border-bottom: solid 1px #ccc;
+        }
+        article{
+          margin-top: 8px;
+        }
+      `}</style>
+    </>
   );
 };
 
 export default postsShow;
 
-export const getServerSideProps:GetServerSideProps<any,{id:string}> = async (context)=>{
-  const connection = await getDatabaseConnection()
-  const post = await connection.manager.findOne(Post,context.params.id)
-  return{
-    props:{
-      post:JSON.parse(JSON.stringify(post))
+export const getServerSideProps: GetServerSideProps<any, { id: string }> = async (context) => {
+  const connection = await getDatabaseConnection();
+  const post = await connection.manager.findOne(Post, context.params.id);
+  return {
+    props: {
+      post: JSON.parse(JSON.stringify(post))
     }
-  }
-}
+  };
+};
