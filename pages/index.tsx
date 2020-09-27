e38c38c65b4d904309e1bb83ef69da0eac03d858
Burt <2902978956@qq.com>
2020-09-27 20:23:59 +0800

    添加首页及其CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,43 @@
-import PostsIndex, {getServerSideProps} from './posts';
+import {NextPage} from 'next';
+import React from 'react';
+import Link from 'next/link';
 
-export default PostsIndex;
-export {getServerSideProps};
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className="cover">
+        <img src="/logo.png" alt="logo"/>
+        <h1>Burt 的个人博客</h1>
+        <div className="content">
+          <p>我是一个爱学习的人</p>
+          <p><Link href="/posts"><a>文章列表</a></Link></p>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .cover{
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        }
+        .cover img{
+          width: 24vw;
+        }
+        .cover h1{
+          margin-top: 0.5em;
+        }
+        .cover .content{
+          margin-top: 1em;
+        }
+        .cover .content p {
+          margin-bottom: 0.5em;
+          text-align: center;
+        }
+        `}</style>
+    </>
+  );
+};
+
+export default Home;
