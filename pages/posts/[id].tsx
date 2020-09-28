e87f09ce7b360a6b3d529886eb977cf62ae46e9c
Burt <2902978956@qq.com>
2020-09-28 11:20:50 +0800

    增加【删除文章】功能

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {GetServerSideProps, GetServerSidePropsContext, NextPage} from 'next';
 import {getDatabaseConnection} from '../../lib/getDatebaseConnection';
 import {Post} from '../../src/entity/Post';
 import marked from 'marked';
 import withSession from '../../lib/withSession';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
+import axios from 'axios'
 
 type Props = {
-  post: Post,
+  id:number;
+  post: Post;
   currentUser: User | null
 }
 const postsShow: NextPage<Props> = (props) => {
-  const {post,currentUser} = props;
+  const {post,currentUser,id} = props;
+  const router =useRouter()
+  const onRemove = useCallback(()=>{
+    axios.delete(`/api/v1/posts/${id}`).then(()=>{
+      window.alert('删除成功')
+      router.push('/posts')
+    },()=>{
+      window.alert('删除失败')
+    })
+  },[id])
   return (
     <>
       <div className="wrapper">
         <header>
           <h1>{post.title}</h1>
           {currentUser &&
-          <p>
+          <p className="actions">
             <Link href="/posts/[id]/edit" as={`/posts/${post.id}/edit`}><a>编辑</a></Link>
+            <button onClick={onRemove}>删除</button>
+            <Link href="/posts" as={`/posts`}><a>文章列表</a></Link>
           </p>
           }
         </header>
@@ -29,6 +43,17 @@ const postsShow: NextPage<Props> = (props) => {
       </div>
 
       <style jsx>{`
+        .actions{
+          font-weight: bold;
+        }
+        .actions  > button{
+          border: none;
+          background:none;
+          color: indianred;
+          margin-left: 16px;
+          margin-right: 32px;
+          cursor: pointer;
+        }
         .wrapper{
           max-width: 800px;
           margin: 16px auto;
@@ -53,10 +78,12 @@ export default postsShow;
 
 export const getServerSideProps: GetServerSideProps<any, { id: string }> = withSession(async (context:GetServerSidePropsContext) => {
   const connection = await getDatabaseConnection();
-  const post = await connection.manager.findOne('Post', context.params.id);
+  const id = context.params.id
+  const post = await connection.manager.findOne('Post', id);
   const currentUser = (context.req as any).session.get('currentUser') || null
   return {
     props: {
+      id:parseInt(id.toString()),
       post: JSON.parse(JSON.stringify(post)),
       currentUser
     }
