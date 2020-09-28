ae6e66adb92045c6eae8aa5f9ef45c96f03426e7
Burt <2902978956@qq.com>
2020-09-28 15:23:12 +0800

    修改 withSession 里 password

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,8 +3,8 @@ import {GetServerSideProps, NextApiHandler} from 'next';
 
 export default function withSession(handler:NextApiHandler | GetServerSideProps) {
   return withIronSession(handler, {
-    password: process.env.SECRET,
-    // password:'11406663056575078404696080695426',
+    // password: process.env.SECRET,
+    password:'11406663056575078404696080695426',
     cookieName: 'blog',
     cookieOptions: {secure:false},
   })
