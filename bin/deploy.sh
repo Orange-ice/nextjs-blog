9798532369dc6ba0e5d5aa8d808c2c048b7dd511
Burt <2902978956@qq.com>
2020-09-28 16:03:04 +0800

    完善 deploy.sh 可以实现一键部署

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,12 +3,12 @@ cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
-git apply migrate.patch;
-yarn compile &&
-yarn m:run &&
-git reset --hard HEAD &&
-docker build -t fang/node-web-app . &&
+# git apply migrate.patch;
+# yarn compile &&
+# yarn m:run &&
+# git reset --hard HEAD &&
+docker build -t burt/node-web-app . &&
 docker kill app &&
 docker rm app &&
-docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
+docker run --name app --network=host -p 3000:3000 -d burt/node-web-app &&
 echo 'OK!'
